117aece0a9349e3a8fc65f7614c95edc64c590cc
odusdegreat <tobiodubote@gmail.com>
2025-07-24 22:21:24 +0100

    dizz

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,9 +1,6 @@
-// components/common/Card.tsx
-import { CardProps } from "@/interfaces";
+// interfaces/index.ts
 
-const Card = ({ title, content }: CardProps) => (
-  <div className="p-4 border rounded-lg shadow-md bg-white dark:bg-zinc-900">
-    <h2 className="text-xl font-semibold mb-2">{title}</h2>
-    <p className="text-gray-700 dark:text-gray-300">{content}</p>
-  </div>
-);
+export interface CardProps {
+  title: string;
+  content: string;
+}
