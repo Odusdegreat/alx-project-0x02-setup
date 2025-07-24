b5f7ee2c02ef4ba8cec79b76e7abfdfbdd0d7a66
odusdegreat <tobiodubote@gmail.com>
2025-07-24 22:44:47 +0100

    commit

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+
+const sizeClasses = {
+  small: "px-2 py-1 text-sm",
+  medium: "px-4 py-2 text-base",
+  large: "px-6 py-3 text-lg",
+};
+
+const Button: React.FC<ButtonProps> = ({
+  label,
+  size = "medium",
+  shape = "rounded-md",
+  onClick,
+  className = "",
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`bg-blue-600 text-white ${sizeClasses[size]} ${shape} ${className}`}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Button from "@/components/common/Button";
 
-const about = () => {
-  return <div>about</div>;
+const AboutPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">About Page</h1>
+
+      <div className="flex gap-4">
+        <Button label="Small Rounded" size="small" shape="rounded-sm" />
+        <Button label="Medium Rounder" size="medium" shape="rounded-md" />
+        <Button label="Large Pill" size="large" shape="rounded-full" />
+      </div>
+    </div>
+  );
 };
 
-export default about;
+export default AboutPage;
