f7ba7090a2d0074847b961a807835c1c0ee3baff
odusdegreat <tobiodubote@gmail.com>
2025-07-24 22:11:38 +0100

    routing

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,11 +1,16 @@
-import React from "react";
-import PropTypes from "prop-types";
+import { useRouter } from "next/router";
 import Link from "next/link";
 
-const Header = (props) => {
-  return <div>Header</div>;
+const Header = () => {
+  const router = useRouter();
+
+  const isHiddenRoute = ["/login", "/register"].includes(router.pathname); // Add "/home" if needed
+
+  if (isHiddenRoute) return null;
+
+  return (
+    <header>
+      <h1>My Header</h1>
+    </header>
+  );
 };
-
-Header.propTypes = {};
-
-export default Header;
